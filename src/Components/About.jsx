891967775de91ac7c29ceccd08297181d0a9e70c
leslie-alldridge <leslie.alldridge@gmail.com>
2018-11-01 07:27:41 +1300

    testing out pics

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Image } from 'react-bootstrap';
 export default class About extends Component {
   render() {
     return (
       <Row className="show-grid">
         <Col xs={4} md={4}>
-          About
+          <Image id="aboutImg" src="/about.png" responsive circle />
         </Col>
         <Col xs={4} md={4}>
           Food &amp; Drink
